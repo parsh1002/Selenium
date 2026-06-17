b491999cee38fa51954ef53828b7c1cd0f1245e3
parsh1002 <158697075+parsh1002@users.noreply.github.com>
2026-06-18 03:27:47 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,19 +2,19 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.Chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        // ChromeOptions options = new ChromeOptions();
+        ChromeOptions options = new ChromeOptions();
 
-        // options.addArguments("--headless=new");
-        // options.addArguments("--no-sandbox");
-        // options.addArguments("--diable-dev-shm-usage");
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--diable-dev-shm-usage");
         
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.practicetestautomation.com/practice-test-login/");
         driver.manage().window().maximize();
         driver.findElement(By.id("username")).sendKeys("student");
